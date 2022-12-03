5e21b6f4ecfae3ea099d77c2c8f96ab03e083b14
Swearfy <swearfy@gmail.com>
2022-12-03 14:32:11 +0000

    check db for user if its also admin then show admin panel

--- app/src/main/java/com/example/surveyapp/MainActivity.kt
+++ app/src/main/java/com/example/surveyapp/MainActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.example.surveyapp.Model.DataBaseHelper
 
 class MainActivity : AppCompatActivity() {
+    val dbHelper: DataBaseHelper = DataBaseHelper(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun login(view :View){
@@ -19,14 +23,25 @@ class MainActivity : AppCompatActivity() {
         var userName = findViewById<EditText>(R.id.text_username).text.toString()
         var passWord = findViewById<EditText>(R.id.text_Password).text.toString()
 
+        val actualUsername = dbHelper.getUser(userName)
+
+
+
+
         if (userName.isBlank() && passWord.isBlank()){
             Toast.makeText(this,"Please Fill in the boxes above", Toast.LENGTH_LONG).show()
         }else if (userName.isBlank() ){
             Toast.makeText(this,"Please Fill in username", Toast.LENGTH_LONG).show()
         }else if (passWord.isBlank() ){
             Toast.makeText(this,"Please Fill in password", Toast.LENGTH_LONG).show()
-        }else if(userName == "yaes" && passWord=="yaes"){
+        }else if(userName == actualUsername.userName && passWord == actualUsername.passWord && actualUsername.isAdmin == 1){
             startActivity(intent)
         }
+    }
+
+
+    fun register(view: View){
+        val intent = Intent(this,RegisterPanel::class.java)
+        startActivity(intent)
     }
 }
