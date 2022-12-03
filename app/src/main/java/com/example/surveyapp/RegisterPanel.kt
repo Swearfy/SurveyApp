864412d400c8dd7728a8a7beedaa37636b59a0af
Swearfy <swearfy@gmail.com>
2022-12-03 19:19:11 +0000

    validation for admin and user registration

--- app/src/main/java/com/example/surveyapp/RegisterPanel.kt
+++ app/src/main/java/com/example/surveyapp/RegisterPanel.kt
@@ -6,8 +6,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Switch
+import android.widget.Toast
+import com.example.surveyapp.Model.DataBaseHelper
+import com.example.surveyapp.Model.User
 
 class RegisterPanel : AppCompatActivity() {
+
+    val dbHelper: DataBaseHelper = DataBaseHelper(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_panel)
@@ -27,10 +33,114 @@ class RegisterPanel : AppCompatActivity() {
 
             }
         }
-
     }
 
+    fun register(view: View) {
 
+        val username = findViewById<EditText>(R.id.text_UserNameRegister).text.toString()
+        val password = findViewById<EditText>(R.id.text_Password1).text.toString()
+        val password2 = findViewById<EditText>(R.id.text_Password2).text.toString()
+        val accescode = findViewById<EditText>(R.id.accesCode).text.toString()
+
+        val switch = findViewById<Switch>(R.id.btn_SwitchLogin)
+
+
+        if (switch.text == "Admin") {
+            if (username.isBlank() && password.isBlank() && password2.isBlank() && accescode.isBlank()) {
+                Toast.makeText(this, "Please fill in the boxes", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (username.isBlank()) {
+                Toast.makeText(this, "Please enter user name", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (password.isBlank()) {
+                Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (password2.isBlank()) {
+                Toast.makeText(this, "Please re-enter password", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (accescode.isBlank()) {
+                Toast.makeText(this, "Please enter the admin code", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (password != password2) {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (accescode.toInt() != 1234) {
+                Toast.makeText(this, "You have entered the wrong access code", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            if (password == password2 && accescode.toInt() == 1234) {
+
+                val checkuser = dbHelper.getUser(username).userName
+                val checkpassword = dbHelper.getUser(username).passWord
+
+                if(checkuser == username && checkpassword == password2 || checkuser == username && checkpassword != password2 ){
+                    Toast.makeText(this,"User already exists",Toast.LENGTH_SHORT).show()
+                }else{
+
+                val user = User(0, username, password2, 1)
+
+                if (dbHelper.addUser(user)) {
+                    Toast.makeText(this, "User added", Toast.LENGTH_SHORT).show()
+                    findViewById<EditText>(R.id.text_UserNameRegister).text.clear()
+                    findViewById<EditText>(R.id.text_Password1).text.clear()
+                    findViewById<EditText>(R.id.text_Password2).text.clear()
+                    findViewById<EditText>(R.id.accesCode).text.clear()
+                } else {
+                    Toast.makeText(this, "Error: The user not added", Toast.LENGTH_SHORT).show()
+                }
+                }
+            }
+        } else if (switch.text == "User") {
+            if (username.isBlank() && password.isBlank() && password2.isBlank()) {
+                Toast.makeText(this, "Please fill in the boxes", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (username.isBlank()) {
+                Toast.makeText(this, "Please enter user name", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (password.isBlank()) {
+                Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (password2.isBlank()) {
+                Toast.makeText(this, "Please re-enter password", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (password != password2) {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (password == password2) {
+
+                val checkuser = dbHelper.getUser(username).userName
+                val checkpassword = dbHelper.getUser(username).passWord
+
+                if(checkuser == username && checkpassword == password2 || checkuser == username && checkpassword != password2 ){
+                    Toast.makeText(this,"User already exists",Toast.LENGTH_SHORT).show()
+                }else{
+                    val user = User(0, username, password2, 0)
+
+                    if (dbHelper.addUser(user)) {
+                        Toast.makeText(this, "User added", Toast.LENGTH_SHORT).show()
+                        findViewById<EditText>(R.id.text_UserNameRegister).text.clear()
+                        findViewById<EditText>(R.id.text_Password1).text.clear()
+                        findViewById<EditText>(R.id.text_Password2).text.clear()
+                    } else {
+                        Toast.makeText(this, "Error: The user not added", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+    }
     fun goBackToLoging(view: View){
         val intent = Intent(this,MainActivity::class.java)
 
